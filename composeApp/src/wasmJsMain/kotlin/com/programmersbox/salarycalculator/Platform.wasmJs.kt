a58175b6a6db8c26330974445dccc87bbbcaf343
jacobrein <jacob.rein@gabb.com>
2024-08-15 07:52:00 -0600

    Refactor currency formatting function
    Updated the `formatCurrency` function for better formatting by handling decimals appropriately and adding commas for thousands separation. This enhances the readability of currency values.

--- composeApp/src/wasmJsMain/kotlin/com/programmersbox/salarycalculator/Platform.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/programmersbox/salarycalculator/Platform.wasmJs.kt
@@ -1,9 +1,21 @@
 package com.programmersbox.salarycalculator
 
-class WasmPlatform: Platform {
+import kotlin.math.roundToInt
+
+class WasmPlatform : Platform {
     override val name: String = "Web with Kotlin/Wasm"
 }
 
 actual fun getPlatform(): Platform = WasmPlatform()
 
-actual fun Double.formatCurrency(): String = this.toString().take(2 + decimals)
+actual fun Double.formatCurrency(): String {
+    var decimal = toString().takeLast(2)
+    if (decimal == "00") decimal = "0"
+    val rest = roundToInt()
+        .toString()
+        .reversed()
+        .chunked(3)
+        .joinToString(",")
+        .reversed()
+    return "$$rest.$decimal".replace("..", ".")
+}
