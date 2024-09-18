83c89ddfa000919c82256ba39ffec9dca62c4127
jacobrein <jacob.rein@gabb.com>
2024-09-18 10:25:59 -0600

    Add beta build type and enable edge-to-edge in MainActivity
    Added a new beta build type with specific settings in the `build.gradle.kts`. Also enabled edge-to-edge display mode in `MainActivity` to enhance visual presentation.

--- composeApp/src/androidMain/kotlin/com/programmersbox/salarycalculator/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/programmersbox/salarycalculator/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             App()
         }
